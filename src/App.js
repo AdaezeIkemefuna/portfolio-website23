2986e284937969eb532809bd4ddd3e4e21d17f19
Ada-ikemefuna <avhyrus@gmail.com>
2023-01-03 17:17:26 +0100

    initial folder set up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./assets/logo.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <section className="body">
+      <header className="hero">
+        <nav className="navigation">
+          <section className="navigation__wrapper">
+            <section className="navigation--left">
+              <img src={logo} alt="my logo" className="navigation__logo" />
+            </section>
+            <section className="navigation--right">
+              <a href="#">Home</a>
+              <a href="#">Projects</a>
+              <a href="#">About Me</a>
+            </section>
+          </section>
+        </nav>
+
+        <section className="hero__mid">
+          <section className="hero__mid--left">
+            <h1>hi, i'm adaeze ikemefuna</h1>
+            <aside>
+              Building Scalable <br /> frontend systems
+            </aside>
+          </section>
+          <section className="hero__mid--right">
+            <ul>
+              <li>Software Engineer</li>
+              <li>Content Writer</li>
+            </ul>
+          </section>
+        </section>
+
+        <section className="hero__bottom">
+          <section className="hero__bottom--left">
+            <h1>email</h1>
+            <aside>
+              <a href="#">hamsaikemefuna@gmail.com</a>
+            </aside>
+          </section>
+          <section className="hero__bottom--right">
+            <ul>
+              <li>
+                <a href="#">Github</a>
+              </li>
+              <li>
+                <a href="#">Twitter</a>
+              </li>
+              <li>
+                <a href="#">LinkedIn</a>
+              </li>
+            </ul>
+          </section>
+        </section>
+
+        <section className="bg-animation">
+          <ul className="circles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </section>
       </header>
-    </div>
+    </section>
   );
 }
 
